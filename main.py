a7d5e196267abab19149aea5a894bcb1e6440700
rjindoo <53840281+rjindoo@users.noreply.github.com>
2021-10-22 21:59:23 -0700

    updated code to add words to dataframe

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ import pickle
 
 filename = 'articlesCOVID.pckl'
 pos_tag = ('VERB', 'NOUN', 'PROPN')
-result = []
+all_articles = []
 def get_keywords_eng(doc):
+    result = []
     for token in doc:
         if(token in nlp_eng.Defaults.stop_words or token.text in string.punctuation or token.text == 'chars'):
             continue
         if(token.pos_ in pos_tag):
+            all_articles.append(token.text)
             result.append(token.text)
+    return result
 
 def load_files():
     return pickle.load(open(filename, 'rb'))
@@ -22,23 +25,31 @@ def load_files():
 nlp_eng = spacy.load('en_core_web_lg')
 newsapi = NewsApiClient(api_key='371f18a638d14bdd8267b996d1f58fa9')
 
-temp = newsapi.get_everything(q='coronavirus', language='en', from_param='2021-09-22', to='2021-10-22', sort_by='relevancy', page_size=100)
+temp = newsapi.get_everything(q='coronavirus', language='en', from_param='2021-09-23', to='2021-10-23', sort_by='relevancy', page_size=100)
 pickle.dump(temp['articles'], open(filename, 'wb'))
 
 article_data = []
 for i, article in enumerate(temp['articles']):
     title = article['title']
+    date = article['publishedAt']
     description = article['description']
     content = article['content']
-    article_data.append({'title':title, 'description':description, 'content':content})
+    article_data.append({'title':title, 'date':date[:10], 'description':description, 'content':content})
 
 df = pd.DataFrame(article_data)
 pd.set_option('display.max_rows', df.shape[0]+1)
 df = df.dropna()
 df.head()
 
+results = []
+for content in article_data:
+    doc = nlp_eng(content['content'])
+    results.append([('#' + x[0]) for x in Counter(get_keywords_eng(doc)).most_common(5)])
+df['keywords'] = results
+
 for article in article_data:
     doc = nlp_eng(article['content'])
     get_keywords_eng(doc)
-print(Counter(result).most_common(5))
+print(Counter(all_articles).most_common(5))
+print(df)
 
